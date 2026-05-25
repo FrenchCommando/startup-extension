9742b974848686657a9bb5a2d5fd05f0ae370669
FrenchCommando <martialren@gmail.com>
2026-05-25 17:16:08 -0400

    default url

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Default used only when the user has not saved a URL via the Options page.
 // The live value lives in chrome.storage.local, set by options.html / options.js.
-var DEFAULT_STARTUP_URL = "https://example.com/";
+var DEFAULT_STARTUP_URL = "https://frenchcommando.com/";
 
 async function getStartupUrl() {
   const { startupUrl } = await chrome.storage.local.get("startupUrl");
